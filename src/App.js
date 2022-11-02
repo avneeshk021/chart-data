fc4e3a5502be61ced98c30949a265aa7dae60003
Avneesh Kumar <kaviikumar010@gmail.com>
2022-11-03 00:51:39 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {  ScatterChart,
+  Scatter, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, BarChart, Bar } from 'recharts';
+const pdata = [
+  {
+    "Alcohol": 1,
+    "Malic Acid": 14.23,
+    "Ash": 1.71,
+  },
+  {
+    "Alcohol": 1,
+    "Malic Acid": 13.2,
+    "Ash": 1.78,
+  },
+  {
+    "Alcohol": 1,
+    "Malic Acid": 13.16,
+    "Ash": 2.36,
+  },
+  {
+    "Alcohol": 1,
+    "Malic Acid": 14.37,
+    "Ash": 1.95,
+  },
+  {
+    "Alcohol": 1,
+    "Malic Acid": 13.24,
+    "Ash": 2.59,
+  },
+  {
+    "Alcohol": 1,
+    "Malic Acid": 14.2,
+    "Ash": 1.76,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <h1 className="chart-heading">ScatterChart</h1>
+      <ResponsiveContainer width="100%" aspect={3}>
+        <ScatterChart data={pdata} width={500} height={300} margin={{ top: 5, right: 300, left: 20, bottom: 5 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="Ash" interval={'preserveStartEnd'} tickFormatter={(value) => value } />
+          <YAxis />
+          <Tooltip contentStyle={{ backgroundColor: 'yellow' }} />
+          <Legend />
+          <Scatter type="monotone" dataKey="Malic Acid" stroke="red" activeDot={{ r: 8 }} />
+          <Scatter type="monotone" dataKey="Alcohol" stroke="green" activeDot={{ r: 8 }} />
+        </ScatterChart>
+      </ResponsiveContainer>
+
+      {/* <h1 className="chart-heading">Area Chart</h1>
+      <ResponsiveContainer width="100%" aspect={3}>
+        <AreaChart
+          width={500}
+          height={300}
+          data={pdata}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="Alcohol" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Area type="monotone" dataKey="Malic Acid" stroke="#8884d8" fill="#8884d8" />
+        </AreaChart>
+      </ResponsiveContainer> */}
+
+      <h1 className="chart-heading">Bar Chart</h1>
+      <ResponsiveContainer width="100%" aspect={3}>
+        <BarChart
+          width={500}
+          height={300}
+          data={pdata}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="Ash" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="Malic Acid" fill="#8884d8" />
+          <Bar dataKey="Alcohol" fill="#82ca9d" />
+        </BarChart>
+      </ResponsiveContainer>
+    </>
   );
 }
 
